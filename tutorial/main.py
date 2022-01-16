e88c9b836c3223773b371595f17e49b9746f12e2
Eduardo Flores <efloresambrosio@gmail.com>
2022-01-15 20:15:55 -0600

    new: predefined values for path variables

--- tutorial/main.py
+++ tutorial/main.py
@@ -1,5 +1,10 @@
-import item as item
+from enum import Enum
 from fastapi import FastAPI
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
 
 app = FastAPI() # FastAPI instance
 
@@ -17,3 +22,14 @@ async def say_hello(name: str):
 @app.get("/items/{item_id}")
 async def read_item(item_id: int):
     return {"item_id": item_id}
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name == ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
