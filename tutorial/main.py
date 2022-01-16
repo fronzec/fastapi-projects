b7f13bb302bc9df3ad7cd56bb333fedf7981c251
Eduardo Flores <efloresambrosio@gmail.com>
2022-01-15 22:01:01 -0600

    new: Multiple path and query parameters

--- tutorial/main.py
+++ tutorial/main.py
@@ -53,3 +53,17 @@ async def get_model(model_name: ModelName):
 @app.get("/files/{file_path:path}")
 async def read_file(file_path: str):
     return {"file_path": file_path}
+
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+    user_id: int, item_id: str, q: Optional[str] = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
