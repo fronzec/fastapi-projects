3f640e3fb54cca3f906ef58d84e02da39e557049
Eduardo Flores <efloresambrosio@gmail.com>
2022-01-15 20:06:59 -0600

    new: path params for endpoints

--- tutorial/main.py
+++ tutorial/main.py
@@ -1,3 +1,4 @@
+import item as item
 from fastapi import FastAPI
 
 app = FastAPI() # FastAPI instance
@@ -11,3 +12,8 @@ async def root(): # path operation function
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id):
+    return {"item_id": item_id}
